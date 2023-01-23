a8984189247d7b9faade5184acb43b6a2e9e569c
Rapha <phalado@gmail.com>
2023-01-23 18:47:46 -0300

    Add new word file

--- lib/load_file.rb
+++ lib/load_file.rb
@@ -2,14 +2,18 @@
 
 class LoadFile
   def parse
-    ('a'..'z').each do |l|
-      path = "lib/word_list_#{l}.txt"
-      load_file(path)
-    end
+    # ('a'..'z').each do |l|
+    #   path = "lib/word_list_#{l}.txt"
+    #   load_file(path)
+    # end
+
+    load_file("lib/palavras.txt")
   end
 
   def load_file(path)
     file = File.open(path, 'r')
-    file.each { |f| Word.new(word: f.split("\n").first).save }
+    file.each do |line|
+      Word.new(word: line.split("\n").first).save if line.length == 5
+    end
   end
 end
